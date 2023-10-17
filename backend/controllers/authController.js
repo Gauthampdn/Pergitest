8d3f254e1eede98f9e16d43bc06e5c51c260b9fc
Gautham P <gauthampdn@gmail.com>
2023-10-17 15:22:09 -0700

    pooptry

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: `${process.env.REACT_APP_API_BACKEND}/undefined/auth/redirect/google`,
+  callbackURL: `${process.env.REACT_APP_API_BACKEND}/poop/auth/redirect/google`,
   passReqToCallback: true
 },
 
