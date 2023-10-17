02b94e58117a66010f4be0b6a66db7755e41f615
Gautham P <gauthampdn@gmail.com>
2023-10-17 16:06:16 -0700

    change

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const passport = require("passport");
 
 
 
+
 const cors = require('cors');
 
 // express app
@@ -24,17 +25,8 @@ app.use(express.json()) // to get req body
 
 
 
-const allowedOrigins = ['http://localhost:3000', 'https://your-production-url.com'];
-
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   credentials: true
 }));
