8ed7a8e68b60394c9fa0e0c9dfce88c7e7cf2c7f
Gautham P <gauthampdn@gmail.com>
2023-10-18 12:16:07 -0700

    c

--- backend/server.js
+++ backend/server.js
@@ -45,8 +45,8 @@ app.use(session({
   secret: 'keyboard cat',
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
-    sameSite: "none",
-    secure: "true"
+    sameSite: "None",
+    secure: true
   }
 }));
 
