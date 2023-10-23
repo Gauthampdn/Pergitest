c36cbf84532d80a9a2ae0f206617a1a81ac72b91
Gautham P <gauthampdn@gmail.com>
2023-10-23 16:18:08 -0700

    trying create

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,8 +1,13 @@
+import Navbar from "../components/Navbar";
 import TemplateForm from "../components/TemplateForm";
 
 const Create = () => {
   return ( 
+   <div>
+      <Navbar/>
       <TemplateForm/>
+   </div>
+
    );
 }
  
