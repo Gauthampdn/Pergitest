515c2a416f3721813679390ceee453b7fca5a743
Gautham P <gauthampdn@gmail.com>
2023-10-23 14:04:56 -0700

    mongostore

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-
 // environment vars
 require("dotenv").config()
-
 
 const express = require("express")
 const mongoose = require("mongoose")
@@ -9,67 +7,52 @@ const templateRoutes = require("./routes/templates")
 const openaiRoutes = require("./routes/openai")
 const authRoutes = require("./routes/auth")
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const passport = require("passport");
-
-
-
 const cors = require('cors');
 
 // express app
 const app = express()
 
-
 // middleware
 app.use(express.json()) // to get req body
-
-
-
 
 app.use(cors({
   origin: 'https://pergi.app',
   credentials: true
 }));
 
+// Connect to the database and set up session store
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.use(session({
+        secret: 'keyboard cat',
+        store: new MongoStore({ mongooseConnection: mongoose.connection }),
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // days hours minutes seconds milli
+            secure: true, // Only use this if you're using HTTPS, set to false if not
+            httpOnly: true,
+        }
+    }));
 
-app.use(session({
-  secret: 'keyboard cat',
-  cookie: {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // days hours minutes seconds milli
-  }
-}));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-
-
-app.use((req, res, next) => {
-  console.log(req.method, req.path)
-  next()
-})
-
-//routes
-app.use("/auth", authRoutes)
-app.use("/api/templates", templateRoutes)
-app.use("/openai", openaiRoutes)
-
-
-
-// connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("connected to DB and listening on the port " + process.env.PORT);
+    app.use((req, res, next) => {
+        console.log(req.method, req.path)
+        next()
     })
-  })
-  .catch((err) => {
+
+    //routes
+    app.use("/auth", authRoutes)
+    app.use("/api/templates", templateRoutes)
+    app.use("/openai", openaiRoutes)
+
+    // Start the server after connecting to the database
+    app.listen(process.env.PORT, () => {
+        console.log("connected to DB and listening on the port " + process.env.PORT);
+    })
+}).catch((err) => {
     console.log(err);
-  })
-
-
-
-// listening on port 
-
+});
