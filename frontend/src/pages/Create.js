6f1f9097cacf8cbb7342761d7dc8d61759d721a9
Gautham P <gauthampdn@gmail.com>
2023-10-27 20:09:07 -0700

    going back

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -2,13 +2,15 @@ import Navbar from "../components/Navbar";
 import TemplateForm from "../components/TemplateForm";
 
 const Create = () => {
-  return ( 
-   <div>
-      <Navbar/>
-      <TemplateForm/>
-   </div>
+   return (
+      <div>
+         <Link to="https://pergi.app">
+            <h1 className="centered-header"> go back </h1>
+         </Link>
+         <TemplateForm />
+      </div>
 
    );
 }
- 
+
 export default Create;
